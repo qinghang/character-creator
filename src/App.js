98f9490fd8319a40ee4f76680bfe0bb1b7c3b984
qinghang <qinghangzeng@gmail.com>
2021-05-04 09:37:10 -0400

    add styles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
+      <div style={{
+        height: '50%',
+        width: '100%',
+        backgroundColor: 'hsl(195deg, 20%, 86%)',
+        position: 'fixed',
+        bottom: 0,
+        left: 0
+      }}></div>
       <CharacterEditor />
       <Footer />
     </>
